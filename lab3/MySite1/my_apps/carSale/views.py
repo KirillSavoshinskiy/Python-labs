1bdcf02bfe263ea600c29345c850c90f4cd3ae28
KirillSavoshinskiy <48174791+KirillSavoshinskiy@users.noreply.github.com>
2020-04-27 22:52:39 +0300

    Added logging and extended user model

--- lab3/MySite1/my_apps/carSale/views.py
+++ lab3/MySite1/my_apps/carSale/views.py
@@ -7,29 +7,32 @@ from django.views import generic
 from django.views.generic import FormView
 from .forms import LoginForm, CarForm
 from .models import Car
+import logging
+
+logging.basicConfig(filename="log-file.log", level=logging.INFO)
 
 
 class AutoListView(generic.ListView):
     model = Car
     template_name = 'carSale/Home.html'
+    logging.info('Car list is presented')
 
 
 class AutoDetailView(generic.DetailView):
     model = Car
     template_name = 'carSale/DetailCarInfo.html'
+    logging.info('Detail car information')
 
 
 class RegisterFormView(FormView):
     form_class = UserCreationForm
     template_name = 'carSale/register.html'
-    success_url = ''
+    success_url = '/'
 
     def form_valid(self, form):
+        logging.info('Register form is valid')
         form.save()
         return super(RegisterFormView, self).form_valid(form)
-
-    def form_invalid(self, form):
-        return super(RegisterFormView, self).form_invalid(form)
 
 
 class LoginFormView(FormView):
@@ -38,12 +41,14 @@ class LoginFormView(FormView):
     success_url = "/"
 
     def form_valid(self, form):
+        logging.info('Login form is valid')
         self.user = form.get_user()
         login(self.request, self.user)
         return super(LoginFormView, self).form_valid(form)
 
 
 def user_logout(request):
+    logging.info('Logout')
     auth.logout(request)
     return redirect('Home')
 
@@ -54,5 +59,6 @@ def new_car(request):
         car_form = CarForm(request.POST, request.FILES)
         if car_form.is_valid():
             car_form.save()
+            logging.info('Added new car')
         return redirect('Home')
     return render(request, 'carSale/newCar.html', {'car_form': car_form})
