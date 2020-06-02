81b9308798c3a755238cd169871761cd94aad25e
KirillSavoshinskiy <48174791+KirillSavoshinskiy@users.noreply.github.com>
2020-06-02 20:28:52 +0300

    Update settings.py

--- lab3/MySite/MySite/settings.py
+++ lab3/MySite/MySite/settings.py
@@ -17,7 +17,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'savosh28@gmail.com'
-EMAIL_HOST_PASSWORD = '12436587'
+ 
 DEFAULT_FROM_EMAIL = 'savosh28@gmail.com'
 DEFAULT_TO_EMAIL = 'savosh28@gmail.com'
 
